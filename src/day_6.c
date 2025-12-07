02bb4721cd03dd091b526fcc549aaf15b7c3eaff
frhel <freyr.helga@gmail.com>
2025-12-07 08:53:43 +0100

    Add comments on day 6

--- src/day_6.c
+++ src/day_6.c
@@ -25,78 +25,89 @@ void load_data(struct Math *math) {
     while (fgets(line, sizeof(line), stdin) != NULL) {
         // Remove trailing newline
         line[strcspn(line, "\n")] = 0;
-            col = 0;
-            if (line[0] != '*' && line[0] != '+') continue;
+        col = 0;
+        if (line[0] != '*' && line[0] != '+') continue;
 
-            col_len = 0;
-            size_t str_len = strlen(line);
+        col_len = 0;
+        size_t str_len = strlen(line);
 
-            // Track whether we are starting a new column
-            bool next = true;
-            for (size_t i = 0; i < str_len; i++) {
-                // When we're starting a new column(next is true)
-                // we save the symbol and disable the switch until
-                // the next symbol. Also increment the column count
-                if (next) {
-                    math->symbols[col] = line[i];
-                    next = !next;
-                    col++;
-                }
-
-                // Increment the current column length so we can save it
-                // to the length array for quick access later
-                col_len++;
-
-                // If the next character is outside the range
-                // of the input we save the last column's size
-                // and break the loop
-                if (i+1 >= str_len) {
-                    math->col_len[col - 1] = col_len + 1;
-                    break;
-                }
-                
-                // If we're still inside the range of the input
-                // we check if the upcoming char is an operation
-                // and if so, we set the flag to start a new
-                // column on the next iteration. In addition we
-                // save the column length and reset it for the next
-                // round.
-                if (line[i+1] == '*' || line[i+1] == '+') {
-                    next = !next;
-                     // Take into account extra space between columns
-                     // And that we have already incremented the column
-                     // count
-                    math->col_len[col - 1] = col_len - 1;
-                    col_len = 0;
-                }
+        // Track whether we are starting a new column
+        bool next = true;
+        for (size_t i = 0; i < str_len; i++) {
+            // When we're starting a new column(next is true)
+            // we save the symbol and disable the switch until
+            // the next symbol. Also increment the column count
+            if (next) {
+                math->symbols[col] = line[i];
+                next = !next;
+                col++;
             }
+
+            // Increment the current column length so we can save it
+            // to the length array for quick access later
+            col_len++;
+
+            // If the next character is outside the range
+            // of the input we save the last column's size
+            // and break the loop
+            if (i+1 >= str_len) {
+                math->col_len[col - 1] = col_len + 1;
+                break;
+            }
+            
+            // If we're still inside the range of the input
+            // we check if the upcoming char is an operation
+            // and if so, we set the flag to start a new
+            // column on the next iteration. In addition we
+            // save the column length and reset it for the next
+            // round.
+            if (line[i+1] == '*' || line[i+1] == '+') {
+                next = !next;
+                    // Take into account extra space between columns
+                    // And that we have already incremented the column
+                    // count
+                math->col_len[col - 1] = col_len - 1;
+                col_len = 0;
+            }
+        }
         row++;
     }
+
     math->col_count = col;
+    
+    // Go back to the start of the input to read in the numbers
     rewind(stdin);
 
+    // Reset the row counter
     row = 0;
-
     while (fgets(line, sizeof(line), stdin) != NULL) {
-        line[strcspn(line, "\n")] = 0;
+        line[strcspn(line, "\n")] = 0; // Get rid of trailing newline
+        // Break the loop when we reach the symbols ros
         if (line[0] == '*' || line[0] == '+') break;
+
         size_t start = 0; // start reading columns at this index
         size_t width;
+        size_t str_idx;
         char str[5]; // Add one for null termination
         for (size_t col = 0; col < math->col_count; col++) {
-            width = math->col_len[col]; // Current column width
-            size_t str_idx = 0; // Start counting a new string
+            // Set the current column width
+            width = math->col_len[col];
+            str_idx = 0; // Start counting a new string
+
             // String in column will be the size of the column width
             for (size_t s = 0; s < width; s++) {
-                char ch = line[start + s]; // Current character in raw input
+                // Current character from the input string
+                char ch = line[start + s];
+
                 // Add the character to the new string in the right place
-                // Change any spaces to dots.
+                // Change any spaces to dots to preserve paddings visually
                 str[str_idx++] = (ch == ' ') ? '.' : ch;
             }
-            str[str_idx] = '\0'; // Terminate the string
+            // Add a null terminator to the string
+            str[str_idx] = '\0'; 
             strcpy(math->numbers[row][col], str); // Copy the string to the struct
 
-            // Set the starting index of the next column within the raw input
+            // Set the starting index of the next column within the input string
             start = start + width + 1;
         }
         row++;
@@ -112,9 +123,9 @@ long long do_op(long long a, long long b, char symbol) {
     return a + b;
 }
 
-int str_to_int(char str[5]) {
+long long str_to_int(char str[5]) {
     while (*str == '.') str++;
-    return atoi(str);
+    return atoll(str);
 }
 
 long long solve_part1(struct Math *math) {
@@ -122,13 +133,15 @@ long long solve_part1(struct Math *math) {
     // Go through on a column by column basis instead of rows
     for (size_t col = 0; col < math->col_count; col++) {
         long long col_total = 0;
+
         // Process each row entry for the current column
         for (size_t row = 0; row < math->row_count; row++) {
             // The number is stored as a string with dots as padding
             // So we do a converstion to int that discards the dots
-            int temp_nr = str_to_int(math->numbers[row][col]);
+            // int temp_nr = str_to_int(math->numbers[row][col]);
             // Typecast to long long
-            long long nr = (long long)temp_nr;
+            long long nr = str_to_int(math->numbers[row][col]);
+
             // Grab the op symbol for the current column
             char op = math->symbols[col];
 
@@ -143,13 +156,17 @@ long long solve_part1(struct Math *math) {
 
 long long solve_part2(struct Math *math) {
     long long total = 0;
+
     // Create a temporary array to hold the rotated column for any given number
     int temp[4][4]; 
+
     // We are going to go through column by column again
     for (size_t col = 0; col < math->col_count; col++) {
         long long col_total = 0;
         memset(temp, 0, sizeof(temp)); // Reset the array
-        char col_symbol = math->symbols[col]; // Grab this column's symbol
+
+        // Grab the arithmetic symbol for this column
+        char col_symbol = math->symbols[col];
         
         // Loop through the size of the column backwards
         // and grab every character from each row in that 
@@ -157,18 +174,32 @@ long long solve_part2(struct Math *math) {
         // from this and use it to perform operations.
         for (int col_char = math->col_len[col] - 1; col_char >= 0; col_char--) {
             // We create a new string to hold each new number
+            // length 4 for the string, 1 for the null terminator
             char str[5];
+
+            // Define an idx variable to track the current new string
+            // character position
             size_t idx = 0;
+
             // For every row in current column's character position
             for (size_t row = 0; row < math->row_count; row++) {
                 // Grab the current character
                 char ch = math->numbers[row][col][col_char];
+
                 if (ch == '.') continue; // Skip any dots
-                str[idx++] = ch; // Save the character to the new string
+                // Save the character to the new string
+                // Increment the string index counter at the same time.
+                // ++idx would increment before using the variable
+                // idx++ uses the variable before incrementing.
+                str[idx++] = ch;
             }
-            str[idx] = '\0'; // Add a terminating character
-            long long nr = atoll(str); // convert to long long
-            col_total = do_op(col_total, nr, col_symbol); // Perform the op
+            // Null terminate the string
+            str[idx] = '\0';
+            long long nr = atoll(str); // Convert the new string to long long
+
+            // Perform the arithmetic operation and 
+            // add the result to the total for this column
+            col_total = do_op(col_total, nr, col_symbol);
         }
         total += col_total;
     }
